31592f3dcaf4ee4e530e8525571c07e89d8c60fd
Winston <tabotabowinston@gmail.com>
2024-11-25 19:21:23 +0800

    added Concept UI

---
+++ MainPageUI.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main</title>
+</head>
+<body>
+    <h1>Select Building</h1>
+    <button>CCS</button>
+    <button>CSM</button>
+    <button>Engineering</button>
+    <button>CLA</button>
+    <button>Nursing</button>
+    <br><br><br>
+    <br><br><br>
+    <br><br><br>
+    <br><br><br>
+    <p>after clicking the button itong page mag labas:   .... (CCS button clicked)</p>
+    <h3>Available Rooms Currently</h3>
+    <table>
+        <thead>
+            <tr>
+                <td>Room name</td>
+                <td>Room Status</td>
+                <td>check room schedule</td>
+                <td>idk something something</td>
+                <td>idk something something</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>LR-1</td>
+                <td>Available</td>
+                <td><a href="datatables.php">Check</a></td>
+                <td>bruh</td>
+                <td>bruh</td>
+            </tr>
+            <tr>
+                <td>LR-1</td>
+                <td>Available</td>
+                <td><a href="datatables.php">Check</a></td>
+                <td>bruh</td>
+                <td>bruh</td>
+            </tr>
+            <tr>
+                <td>LR-1</td>
+                <td>Available</td>
+                <td><a href="datatables.php">Check</a></td>
+                <td>bruh</td>
+                <td>bruh</td>
+            </tr>
+        </tbody>
+    </table>
+
+</body>
+</html>
