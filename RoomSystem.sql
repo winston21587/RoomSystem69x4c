817828bb414661a34374d885df82a349f8ac3f37
Winston <152620905+winston21587@users.noreply.github.com>
2024-11-27 09:19:12 +0800

    updated database
    added the sched and room table with relation

--- RoomSystem.sql
+++ RoomSystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 19, 2024 at 05:36 AM
+-- Generation Time: Nov 27, 2024 at 02:16 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,19 +24,136 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Department`
+--
+
+CREATE TABLE `Department` (
+  `id` int(11) NOT NULL,
+  `deptName` varchar(100) NOT NULL,
+  `location` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Department`
+--
+
+INSERT INTO `Department` (`id`, `deptName`, `location`) VALUES
+(1, 'CCS-College of Computing Science', 'Campus B'),
+(2, 'CSM', 'campus-b'),
+(3, 'CLA-', 'campus a');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Room`
+--
+
+CREATE TABLE `Room` (
+  `id` int(11) NOT NULL,
+  `RoomName` varchar(100) NOT NULL,
+  `department` varchar(100) NOT NULL,
+  `status` enum('unavailable','available') NOT NULL DEFAULT 'unavailable',
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Room`
+--
+
+INSERT INTO `Room` (`id`, `RoomName`, `department`, `status`, `timestamp`) VALUES
+(1, 'LR-1', 'CCS', 'unavailable', '2024-11-27 01:08:59'),
+(2, 'LR-2', 'CCS', 'unavailable', '2024-11-27 01:09:31'),
+(3, 'LR-3', 'CCS', 'unavailable', '2024-11-27 01:09:31'),
+(4, 'CLA-6', 'CLA', 'unavailable', '2024-11-27 01:10:07'),
+(5, 'CSM-11', 'CSM', 'unavailable', '2024-11-27 01:10:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `id` int(11) NOT NULL,
+  `roomid` int(11) NOT NULL,
+  `DayOfWeek` enum('moday','tuesday','wednesday','thursday','friday','saturday','sunday') NOT NULL,
+  `start-time` time NOT NULL,
+  `end-time` time NOT NULL,
+  `subject` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `schedule`
+--
+
+INSERT INTO `schedule` (`id`, `roomid`, `DayOfWeek`, `start-time`, `end-time`, `subject`) VALUES
+(1, 1, 'moday', '07:00:00', '08:00:00', 'webdev'),
+(2, 1, 'moday', '10:00:00', '11:30:00', 'programming 1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE `subject` (
+  `id` int(11) NOT NULL,
+  `SubName` varchar(100) NOT NULL,
+  `subcode` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `UserName` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `email` varchar(200) NOT NULL,
-  `password` varchar(200) NOT NULL
+  `course` varchar(100) NOT NULL,
+  `section` varchar(50) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `role` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `course`, `section`, `password`, `role`) VALUES
+(1, 'winston', 'winstontabotabo10@gmail.com', 'BSCS', 'A', '$2y$10$HGyNd3Lbhqv2ZEzl8/BimOBOlTVdeLrTg.2zYYXr1Ytw9yCktnBsa', ''),
+(2, 'alken', 'alken@email.com', 'BSCS', 'A', '$2y$10$DcWcPwlGtuo.3oasX3YCeOnOEbY7CLWHFc.aCh97scDdHQFzgHkTa', '');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `Department`
+--
+ALTER TABLE `Department`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Room`
+--
+ALTER TABLE `Room`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `schedule_ibfk_1` (`roomid`);
+
+--
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -49,10 +166,44 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `Department`
+--
+ALTER TABLE `Department`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `Room`
+--
+ALTER TABLE `Room`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `schedule`
+--
+ALTER TABLE `schedule`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `subject`
+--
+ALTER TABLE `subject`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`roomid`) REFERENCES `Room` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
