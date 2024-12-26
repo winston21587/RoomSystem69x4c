cca9b89849b1d3e271537ab3a7bcd0d565394ff3
Winston <tabotabowinston@gmail.com>
2024-12-26 20:32:41 +0800

    q

--- modals/scheduleInfo.php
+++ modals/scheduleInfo.php
@@ -1,3 +1,18 @@
 <div>
-    
+    <h1>Schedule: <?= //day  ?> </h1>
+
+    <p>schedule: <?= //sched ?></p>
+
+    <div>
+        <p></p>
+        <p>to</p>
+        <p></p>
+    </div>
+    <div>
+        <p><?= //schoolyear ?></p>
+        <p><?= //semester ?></p>
+    </div>
+    <div>
+        <p><?= status ?></p>
+    </div>
 </div>
